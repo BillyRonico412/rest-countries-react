2bb9d8cfdf556c7f79191a4282f747784610b5b9
BillyRonico <billyronico412@gmail.com>
2022-04-04 08:52:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# rest-countries-react
+Le projet de rest countries API de front end mentor codé en React
+https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca/hub/rest-countries-api-with-color-theme-switcher-7SFr5lkuI
+
+## Utilisation de React
+- TypeScript
+- Hooks
+- React Router
+- Docker
+- Testing (A voir)
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "hsl(209, 23 %, 22 %)",
+        "very-dark-blue-1": "hsl(207, 26 %, 17 %)",
+        "vary-dark-blue-2": "hsl(200, 15 %, 8 %)",
+        "dark-gray": " hsl(0, 0 %, 52 %)",
+        "very-light-gary": "hsl(0, 0 %, 98 %)",
+      },
+      fontSize: {
+        "petit": "14px",
+        "normal": "16px",
+      }
+    },
+  },
+  plugins: [],
+}
